6e78302bd0fa873e9ba30e1d5e3466850c5c4f9a
Dusan Baran <dbaran@hotmail.sk>
2017-03-09 23:06:47 +0100

    Add desktop enviroments and display manager

--- archon.sh
+++ archon.sh
@@ -2,24 +2,76 @@
 set -x
 
 function aur_install() {
-  curl -o $1.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
-  tar -xvzf $1.tar.gz
-  cd $1
-  makepkg
-  sudo pacman -U $1-*.pkg.tar.xz
-  cd ..
+  if which yaourt; then
+    yaourt -S $@ --noconfirm
+  else
+    for PKG in $@; do
+      curl -o $PKG.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/$PKG.tar.gz
+      tar -xvzf $PKG.tar.gz
+      cd $PKG
+      makepkg
+      sudo pacman -U $PKG-*.pkg.tar.xz
+      cd ..
+    done
+  fi
+}
+
+function pacman_install() {
+  sudo pacman -S $@ --noconfirm
+}
+
+function service_enable() {
+  sudo systemctl enable "$@"
 }
 
 function install_yaourt() {
-  sudo pacman -S yajl
+  pacman_install yajl
   aur_install package-query
   aur_install yaourt
+}
+
+function install_xorg() {
+  pacman_install xorg
+}
+
+function install_bspwm() {
+  pacman_install bspwm
+  aur_install lemonbar-xft-git
+}
+
+function install_gnome() {
+  pacman_install gnome
+  service_enable gdm
+}
+
+function install_lxdm() {
+  pacman_install lxdm
+  service_enable lxdm
+}
+
+function install_pulseaudio() {
+  pacman_install pulseaudio pulseaudio-alsa
 }
 
 while [ $# -gt 0 ]; do
   case "$1" in
     -y)
       install_yaourt
+      ;;
+    -x)
+      install_xorg
+      ;;
+    -b)
+      install_bspwm
+      ;;
+    -g)
+      install_gnome
+      ;;
+    -l)
+      install_lxdm
+      ;;
+    -p)
+      install_pulseaudio
       ;;
   esac
   shift
